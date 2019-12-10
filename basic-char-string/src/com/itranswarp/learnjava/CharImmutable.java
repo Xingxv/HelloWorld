3ea9c0fec1813a5f7becd1e7aa2c0693313e86d1
Xingxv <1181424337@qq.com>
2019-12-10 22:24:30 +0800

    字符型

---
+++ basic-char-string/src/com/itranswarp/learnjava/CharImmutable.java
@@ -0,0 +1,14 @@
+package com.itranswarp.learnjava;
+/**
+ *  字符不可变
+ * @author LYW
+ *
+ */
+public class CharImmutable {
+	public static void main(String[] args) {
+        String s = "hello";  //s指向"hello"
+        String t = s;  //t指向s指向的"hello"
+        s = "world";   //s指向"world"
+        System.out.println(t); // t是"hello"还是"world"?  t是"hello"
+    }
+}
