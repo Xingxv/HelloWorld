31cd1d4e91bdf6aeaab660e3b915e2b35b83a6af
Xingxv <1181424337@qq.com>
2019-12-11 19:38:05 +0800

    数组排序

--- array-sort/src/com/itranswarp/learnjava/Main.java
+++ array-sort/src/com/itranswarp/learnjava/Main.java
@@ -11,6 +11,18 @@ public class Main {
 		// 排序前:
 		System.out.println(Arrays.toString(ns));
 		// TODO:
+		//冒泡排序
+		for (int i = 0; i < ns.length-1; i++) {
+			for (int j = 0; j < ns.length-i-1; j++) {
+				if (ns[j]<ns[j+1]) {
+					//交换ns[j]和ns[j+1]
+					int tmp = ns[j];
+					ns[j] = ns[j+1];
+					ns[j+1] = tmp;
+				}
+			}
+			
+		}
 		// 排序后:
 		System.out.println(Arrays.toString(ns));
 		if (Arrays.toString(ns).equals("[96, 89, 73, 65, 50, 36, 28, 18, 12, 8]")) {
