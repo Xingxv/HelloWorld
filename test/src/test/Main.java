95c7a49dae652ae024e87ad44220d24b76860e02
Xingxv <32195743+Xingxv@users.noreply.github.com>
2019-12-12 18:08:58 +0800

    测试

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -1,12 +1,24 @@
 package test;
 
+import java.util.Arrays;
+
 public class Main {
-	public static void main(String[] args) {
-        String s = "Hello";
-        System.out.println(s);
-        s = s.toUpperCase();
-        System.out.println(s);
-        s = s.toLowerCase();
-        System.out.println(s);
+    public static void main(String[] args) {
+        int[] scores = new int[] { 88, 77, 51, 66 };
+        Score s = new Score(scores);
+        s.printScores();
+        scores[2] = 99;
+        s.printScores();
+    }
+}
+
+class Score {
+    private int[] scores;
+    public Score(int[] scores) {
+        this.scores = scores.clone();
+    }
+
+    public void printScores() {
+        System.out.println(Arrays.toString(scores));
     }
 }
