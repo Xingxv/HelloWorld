21f4f565eadfa41a3f116d9a4b92225454b8813e
Xingxv <32195743+Xingxv@users.noreply.github.com>
2019-12-12 15:18:28 +0800

    抽象类

---
+++ oop-abstractclass/src/Income.java
@@ -0,0 +1,14 @@
+
+/**
+ * 定义抽象类Income
+ */
+public abstract class Income {
+
+	// TODO
+	protected double income;
+	public Income(double income) {
+		this.income = income;
+	}
+	
+	public abstract double getTax();
+}
