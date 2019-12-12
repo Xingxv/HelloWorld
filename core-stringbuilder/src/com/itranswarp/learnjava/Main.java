7267874e3939f6bab53da8a841a3dffef8d4cee0
Xingxv <1181424337@qq.com>
2019-12-12 20:54:25 +0800

    核心类-StringBuilder

---
+++ core-stringbuilder/src/com/itranswarp/learnjava/Main.java
@@ -0,0 +1,32 @@
+package com.itranswarp.learnjava;
+
+/**
+ * 核心类-StringBuilder 
+ * 使用StringBuilder构造一个INSERT语句
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		String[] fields = { "name", "position", "salary" };
+		String table = "employee";
+		String insert = buildInsertSql(table, fields);
+		System.out.println(insert);
+		System.out.println(
+				"INSERT INTO employee (name, position, salary) VALUES (?, ?, ?)".equals(insert) ? "测试成功" : "测试失败");
+	}
+
+	static String buildInsertSql(String table, String[] fields) {
+		// TODO:
+		StringBuilder sb = new StringBuilder();
+		sb.append("INSERT INTO ").append(table+" (");
+		for (int i = 0; i < fields.length; i++) {
+			sb.append(fields[i]);
+			if (i<fields.length-1) {
+				sb.append(", ");
+			}
+		}
+		sb.append(") VALUES (?, ?, ?)");
+		return sb.toString();
+	}
+
+}
