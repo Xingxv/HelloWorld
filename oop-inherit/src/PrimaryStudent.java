07103ea8f843ebdb722dc4d7b01cf8d253c26f41
Xingxv <32195743+Xingxv@users.noreply.github.com>
2019-12-12 09:34:12 +0800

    继承

---
+++ oop-inherit/src/PrimaryStudent.java
@@ -0,0 +1,12 @@
+
+public class PrimaryStudent extends Student {
+	
+	protected int grade;
+
+	public PrimaryStudent(String name, int age, int score,int grade) {
+		super(name, age, score);
+		// TODO Auto-generated constructor stub
+		this.grade = grade;
+	}
+
+}
