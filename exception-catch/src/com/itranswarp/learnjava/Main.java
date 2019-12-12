7360696ce975554732c3edf87f584cd5206aa851
Xingxv <1181424337@qq.com>
2019-12-13 01:36:16 +0800

    捕获异常

---
+++ exception-catch/src/com/itranswarp/learnjava/Main.java
@@ -0,0 +1,30 @@
+package com.itranswarp.learnjava;
+
+/**
+ * 捕获异常
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		String a = "12";
+		String b = "x9";
+		// TODO: 捕获异常并处理
+		try {
+			int c = stringToInt(a);
+			int d = stringToInt(b);
+			System.out.println(c * d);
+		} catch (NumberFormatException e) {
+			// TODO: handle exception
+			System.out.println("错误NumberFormatException");
+			e.printStackTrace();
+		}catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("错误Exception");
+//			e.printStackTrace();
+		}
+	}
+
+	static int stringToInt(String s) {
+		return Integer.parseInt(s);
+	}
+}
