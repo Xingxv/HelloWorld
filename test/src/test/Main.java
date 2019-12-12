b36684579f1fbdc89bf417c70085f0f802a39065
Xingxv <1181424337@qq.com>
2019-12-12 22:47:06 +0800

    测试

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -1,16 +1,30 @@
 package test;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IntrospectionException {
         int[] scores = new int[] { 88, 77, 51, 66 };
         Score s = new Score(scores);
         s.printScores();
         scores[2] = 99;
         s.printScores();
+        
+        //通过静态方法valueOf(String)创建Integer实例
         Integer n3 = Integer.valueOf("100");
         System.out.println(n3.intValue());
+        
+        //要枚举一个JavaBean的所有属性
+        BeanInfo info = Introspector.getBeanInfo(Person.class);
+        for (PropertyDescriptor pd : info.getPropertyDescriptors()) {
+            System.out.println(pd.getName());
+            System.out.println("  " + pd.getReadMethod());
+            System.out.println("  " + pd.getWriteMethod());
+        }
     }
 }
 
@@ -25,6 +39,7 @@ class Score {
     }
 }
 
+//JavaBean
 class Person{
 	private String name;
 	private int age;
