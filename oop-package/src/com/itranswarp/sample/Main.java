c0fc46d4ce8bcc7e3ac873e45aa95809444f8faa
Xingxv <32195743+Xingxv@users.noreply.github.com>
2019-12-12 16:24:27 +0800

    包

---
+++ oop-package/src/com/itranswarp/sample/Main.java
@@ -0,0 +1,15 @@
+package com.itranswarp.sample;
+
+import com.itranswarp.world.Person;
+
+/**
+ * 包
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Person p1 = new Person("小明");
+		System.out.println(p1.hello());
+	}
+
+}
