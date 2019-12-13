433d8258ab78b450c7c54c85e351a2aa13e2bcbf
Xingxv <32195743+Xingxv@users.noreply.github.com>
2019-12-13 10:46:47 +0800

    抛出异常

--- exception-throw/src/com/itranswarp/learnjava/Main.java
+++ exception-throw/src/com/itranswarp/learnjava/Main.java
@@ -8,28 +8,28 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			System.out.println(tax(2000, 0.1));
-		} catch (IllegalAccessException e) {
+		} catch (IllegalArgumentException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		try {
 			System.out.println(tax(-200, 0.1));
-		} catch (IllegalAccessException e) {
+		} catch (IllegalArgumentException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		try {
 			System.out.println(tax(2000, -0.1));
-		} catch (IllegalAccessException e) {
+		} catch (IllegalArgumentException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	static double tax(int salary, double rate) throws IllegalAccessException {
+	static double tax(int salary, double rate) throws IllegalArgumentException {
 		// TODO: 如果传入的参数为负，则抛出IllegalArgumentException
 		if (salary<0||rate<0) {
-			throw new IllegalAccessException();
+			throw new IllegalArgumentException();
 		}
 		return salary * rate;
 	}
